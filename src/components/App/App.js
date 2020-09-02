7c15c3009ae33be666889653d4383162cda2e52b
maamunrcd <maamunrcd@gmail.com>
2020-09-03 00:21:07 +0600

    complete initial setup

---
+++ src/components/App/App.js
@@ -0,0 +1,190 @@
+import React from 'react';
+// import logo from './logo.svg';
+import Contact from '../Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grid-demo">
+        <div className="row">
+          <div className="col-12">
+            <span>.col-12</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-11">
+            <span>.col-11</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-10">
+            <span>.col-10</span>
+          </div>
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-9">
+            <span>.col-9</span>
+          </div>
+          <div className="col-3">
+            <span>.col-3</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-8">
+            <span>.col-8</span>
+          </div>
+          <div className="col-4">
+            <span>.col-4</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-7">
+            <span>.col-7</span>
+          </div>
+          <div className="col-5">
+            <span>.col-5</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <span>.col-6</span>
+          </div>
+          <div className="col-6">
+            <span>.col-6</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-5">
+            <span>.col-5</span>
+          </div>
+          <div className="col-7">
+            <span>.col-7</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <span>.col-4</span>
+          </div>
+          <div className="col-8">
+            <span>.col-8</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <span>.col-3</span>
+          </div>
+          <div className="col-9">
+            <span>.col-9</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+          <div className="col-10">
+            <span>.col-10</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-11">
+            <span>.col-11</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+          <div className="col-1">
+            <span>.col-1</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+          <div className="col-2">
+            <span>.col-2</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <span>.col-3</span>
+          </div>
+          <div className="col-3">
+            <span>.col-3</span>
+          </div>
+          <div className="col-3">
+            <span>.col-3</span>
+          </div>
+          <div className="col-3">
+            <span>.col-3</span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4">
+            <span>.col-4</span>
+          </div>
+          <div className="col-4">
+            <span>.col-4</span>
+          </div>
+          <div className="col-4">
+            <span>.col-4</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
