427c521c052ad51b04326d006d2c85877d217bd5
maamunrcd <maamunrcd@gmail.com>
2020-09-05 01:15:44 +0600

    checked responsive all component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,187 +1,29 @@
-import React from 'react';
-// import logo from './logo.svg';
-import Contact from '../Contact';
+import React, {useState, useEffect} from "react";
+import Contact from "../Contact";
+import Home from "../Home";
+import Work from "../Work";
+import "./App.scss";
+import About from "../About";
 
 function App() {
+  const [displayComponent, setDisplayComponent] = useState('contact');
+  const changeContentView = (componentName) => {
+    setDisplayComponent(componentName);
+  };
+  useEffect(() => {
+    console.log(displayComponent, 'hello displayi')
+  }, [displayComponent]);
   return (
     <div className="App">
-      <div className="grid-demo">
-        <div className="row">
-          <div className="col-12">
-            <span>.col-12</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-11">
-            <span>.col-11</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-10">
-            <span>.col-10</span>
-          </div>
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-9">
-            <span>.col-9</span>
-          </div>
-          <div className="col-3">
-            <span>.col-3</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-8">
-            <span>.col-8</span>
-          </div>
-          <div className="col-4">
-            <span>.col-4</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-7">
-            <span>.col-7</span>
-          </div>
-          <div className="col-5">
-            <span>.col-5</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6">
-            <span>.col-6</span>
-          </div>
-          <div className="col-6">
-            <span>.col-6</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-5">
-            <span>.col-5</span>
-          </div>
-          <div className="col-7">
-            <span>.col-7</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-4">
-            <span>.col-4</span>
-          </div>
-          <div className="col-8">
-            <span>.col-8</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-3">
-            <span>.col-3</span>
-          </div>
-          <div className="col-9">
-            <span>.col-9</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-          <div className="col-10">
-            <span>.col-10</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-11">
-            <span>.col-11</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-          <div className="col-1">
-            <span>.col-1</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-          <div className="col-2">
-            <span>.col-2</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-3">
-            <span>.col-3</span>
-          </div>
-          <div className="col-3">
-            <span>.col-3</span>
-          </div>
-          <div className="col-3">
-            <span>.col-3</span>
-          </div>
-          <div className="col-3">
-            <span>.col-3</span>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-4">
-            <span>.col-4</span>
-          </div>
-          <div className="col-4">
-            <span>.col-4</span>
-          </div>
-          <div className="col-4">
-            <span>.col-4</span>
-          </div>
-        </div>
+      <div className="wrapper">
+        {displayComponent === 'work' ? (
+          <Work handleAction={changeContentView}/>
+        ) : displayComponent === 'about' ? (
+          <About handleAction={changeContentView}/>
+        ):displayComponent === 'contact' ? (
+        <Contact handleAction={changeContentView}/>) : (
+          <Home handleAction={changeContentView}/>
+        )}
       </div>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
-
-export default function Contact() {
-    return (
-        <div>
-            Contact From
+import React from "react";
+import "./Contact.scss";
+export default function Contact(props) {
+  return (
+    <div className="root-view">
+      <div className="row">
+        <div className="col-9 center-column">
+          <div className="action-group">
+            <button
+              class="p-button--neutral uppercase"
+              onClick={() => props.handleAction("home")}
+            >
+              <i class="fa fa-arrow-left" aria-hidden="true"></i>
+              Back
+            </button>
+          </div>
+          <div className="content-view">
+            <div className="content-view-list">
+              <h2 className="uppercase">CONTACT</h2>
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent vestibulum risus sit amet lectus condimentum, ac
+                fermentum nibh ullamcorper magna.
+              </p>
+            </div>
+            <form className="contact-from">
+                <div className="form-group">
+                    <label className="uppercase" htmlFor="name">Name</label>
+                    <input className="input-field" type="text" id="name" />
+                </div>
+                <div className="form-group">
+                    <label className="uppercase" htmlFor="email">Email</label>
+                    <input className="input-field" type="email" id="email"/>
+                </div>
+                <div className="form-group">
+                    <label className="uppercase" for="message">Message</label>
+                    <textarea className="input-field" id="message" rows="4"></textarea>
+                </div>
+                <div className="action-group">
+                    <button type="submit" className="uppercase">Send Message</button>
+                </div>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
