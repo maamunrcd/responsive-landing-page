43690804fbef8f615dc43e43b17087e92378fd19
maamunrcd <maamunrcd@gmail.com>
2020-09-05 23:09:52 +0600

    changed config files

---
+++ build/precache-manifest.3b8f5491496d62ccc6160b2a0d6b4f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7493641ad6e2e8d53166544c09d27068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f19286760a75917fe50",
+    "url": "/static/css/main.48347136.chunk.css"
+  },
+  {
+    "revision": "1de516e403d4bfe5a98c",
+    "url": "/static/js/2.99b9e3f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99b9e3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f19286760a75917fe50",
+    "url": "/static/js/main.e16974d1.chunk.js"
+  },
+  {
+    "revision": "37c36dcdfb22975c9cce",
+    "url": "/static/js/runtime-main.41419a3b.js"
+  }
+]);
